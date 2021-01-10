2a140c0a9798deab21ba0cec2380a7d57bfcaa7a
Ajay Chinta <ajay.chinta@gmail.com>
2021-01-10 22:16:51 +0530

    [add] model names parameterized

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from typing import List, Optional
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, APIRouter
 from fastapi.responses import PlainTextResponse
 from fastapi.exceptions import RequestValidationError
+from fastapi.routing import APIRoute
+
 from pydantic import BaseModel, Field
 
 from . import crud, models, schemas
 from .database import engine, get_db
 from .dynamic import create_fast_models
 from sqlalchemy.orm import Session
+from functools import partial
 import uvicorn
 import logging
 
@@ -25,21 +28,34 @@ def read_aims_model_fields(model_name: str, skip: int = 0, limit: int = 100, db:
     result = crud.read_aims_model_fields(model_name)
     return result
 
-country_db, country_pyd = create_fast_models('res.country')
+odoo_models = ['srcm.group','res.country']
 
-class CountryApi2(BaseModel):
-    id: Optional[int] = None
-    # name: Optional[str] = None
-    # column1: Optional[str] = ''
-    # column2: Optional[str] = ''
 
+for odoo_model in odoo_models:
+    db_model, api_model = create_fast_models(odoo_model)
+
+    def read_objects():
+        db = next(get_db())
+        objs = db.query(db_model).all()[0:10]
+        return objs
+
+    route_name = odoo_model.replace('.','_')
+    app.router.add_api_route(f'/odoo/{route_name}', read_objects, methods=['get'],
+                         response_model=List[api_model])
+
+# country_db, country_pyd = create_fast_models('res.country')
 # @app.get('/countries', response_model=List[country_pyd])
-@app.get('/countries', response_model=CountryApi2)
-def read_countries():
-    db = next(get_db())
-    countries = db.query(country_db).all()[:10]
-    logger.info(countries)
-    return countries
+# def read_countries(skip: int = 0, limit: int = 0):
+#     db = next(get_db())
+#     countries = db.query(country_db).all()[skip: skip + limit]
+#     return countries
+
+# group_db, group_pyd = create_fast_models('srcm.group')
+# @app.get('/groups', response_model=List[group_pyd])
+# def read_groups(skip: int = 0, limit: int = 0):
+#     db = next(get_db())
+#     groups = db.query(group_db).all()[skip: skip + limit]
+#     return groups
 
 # if __name__ == '__main__':
     # uvicorn.run(app, host='0.0.0.0', port=8000)
